0081fb1b8c41008814b9607d7e817ac669bf26e5
IchaCoder <ichabordyeboah247@gmail.com>
2026-07-20 15:30:12 +0000

    fixed auth not working

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,12 +1,23 @@
-import { neon } from "@neondatabase/serverless";
 import { env } from "@rag/env/server";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import * as schema from "./schema";
 
+const globalForDb = globalThis as typeof globalThis & {
+  pool?: Pool;
+};
+
+function getPool() {
+  if (!globalForDb.pool) {
+    globalForDb.pool = new Pool({ connectionString: env.DATABASE_URL });
+  }
+
+  return globalForDb.pool;
+}
+
 export function createDb() {
-  const sql = neon(env.DATABASE_URL);
-  return drizzle(sql, { schema });
+  return drizzle(getPool(), { schema });
 }
 
 export const db = createDb();
